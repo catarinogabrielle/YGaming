8ed2031bf164041999614b15c58af322497062c2
catarinogabrielle <catarinogabrielle@gmail.com>
2026-04-08 00:37:45 -0300

    atualização pwa

--- sw.js
+++ sw.js
@@ -2,11 +2,24 @@ const CACHE_NAME = 'ygaming-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
-  // Você pode adicionar os nomes das suas imagens aqui, como '/logo.png'
+  '/manifest.json',
+  '/icon.png',
+  '/favicon.png',
+  '/styles.css',
+  '/app.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns',
+  'https://cdn.jsdelivr.net/npm/date-fns@2.28.0/dist/date-fns.min.js',
+  'https://cdn.jsdelivr.net/npm/date-fns@2.28.0/dist/locale/pt-BR.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-annotation@1.1.0/dist/chartjs-plugin-annotation.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0/dist/chartjs-plugin-datalabels.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom@1.2.1/dist/chartjs-plugin-zoom.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-legend@1.0.0/dist/chartjs-plugin-legend.min.js',
+  'https://cdn.jsdelivr.net/npm/chartjs-plugin-legend@1.0.0/dist/chartjs-plugin-legend.min.css',
 ];
 
-// Instala o service worker e faz o cache dos arquivos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -16,15 +29,14 @@ self.addEventListener('install', event => {
   );
 });
 
-// Intercepta as requisições para carregar mais rápido
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
         if (response) {
-          return response; // Retorna do cache se existir
+          return response;
         }
-        return fetch(event.request); // Puxa da internet se não tiver no cache
+        return fetch(event.request);
       })
   );
 });
